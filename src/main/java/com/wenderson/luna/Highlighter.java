58e61ea14b50f5e89f56ca5da79fc79256cf6d48
Wenderson <wendersonjunior309@gmail.com>
2025-05-10 15:00:09 -0300

    Agora é possivel adicionar realce em outras linguages.

--- src/main/java/com/wenderson/luna/Highlighter.java
+++ src/main/java/com/wenderson/luna/Highlighter.java
@@ -5,89 +5,115 @@ import java.util.regex.*;
 import org.fxmisc.richtext.model.*;
 
 public class Highlighter {
-	private static String[] KEYWORDS = new String[] {
-		"abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
-		"continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float",
-		"for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native",
-		"new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super",
-		"switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while",
-		"var", "module", "requires", "exports"
-	};
+	private Pattern pattern = Pattern.compile("");;
 
-	private static String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
-
-	private static String SEMICOLON_PATTERN = "\\;";
-
-	private static String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
-
-	private static String COMMENT_PATTERN = "//[^\n]*|/\\*(.|\\R)*?\\*/|/\\*[^\\v]*|^\\h*\\*([^\\v]*|/)";
-
-	private static String NUMBER_PATTERN = "[0-9]";
-
-	private static String CLASS_PATTERN = "(?<=\\.)[A-Z]\\w+(?=\\;)|[A-Z]\\w+(?=\\[)|[A-Z]\\w+(?=\\<)|(?<=class\\s)[A-Z]\\w+|(?<=new\\s)[A-Z]\\w+|(?<=extends\\s)[A-Z]\\w+|(?<=implements\\s)[A-Z]\\w+|(?<![a-z]\\w+)[A-Z]\\w+(?=\\.)|[A-Z]\\w+(?=\\s[a-z])";
-
-	private static String CHARS_PATTERN = "=|\\+|-|\\*|\\/|!|&|\\|:|\\>|\\<|\\?";
-
-	private static String BOOLEAN_PATTERN = "true|false";
-
-	private static String SINGLE_QUOTE_STRING_PATTERN = "'(.*?)'";
-
-	private static String FUNCTION_PATTERN = "[a-z]\\w+(?=\\()";
-
-	private static String CONSTANT_PATTERN = "(?-i)[A-Z]+(?![a-z])";
-
-	private static String ANOTATION_PATTERN = "@([A-Z]\\w+|[A-Z])";
-
-	private static Pattern PATTERN = Pattern.compile(
-		"(?<KEYWORD>" + KEYWORD_PATTERN + ")"
-		+ "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
-		+ "|(?<STRING>" + STRING_PATTERN + ")"
-		+ "|(?<COMMENT>" + COMMENT_PATTERN + ")"
-		+ "|(?<NUMBER>" + NUMBER_PATTERN + ")"
-		+ "|(?<CLASS>" + CLASS_PATTERN + ")"
-		+ "|(?<CHARS>" + CHARS_PATTERN + ")"
-		+ "|(?<BOOLEAN>" + BOOLEAN_PATTERN + ")"
-		+ "|(?<SINGLEQUOTESTRING>" + SINGLE_QUOTE_STRING_PATTERN + ")"
-		+ "|(?<FUNCTION>" + FUNCTION_PATTERN + ")"
-		+ "|(?<CONSTANT>" + CONSTANT_PATTERN + ")"
-		+ "|(?<ANOTATION>" + ANOTATION_PATTERN + ")"
-	);
-
-	private String programmingLanguage = "Plain text";
+	private HashMap<String, String> groups = new HashMap<>();
 
 	public void setSyntax(String programmingLanguage) {
-		this.programmingLanguage = programmingLanguage;
+		if (programmingLanguage.equals("Plain text")) {
+			pattern = Pattern.compile("");
+
+			groups.clear();
+		}
+
+		if (programmingLanguage.equals("Java")) {
+			var keywords = new String[] {
+				"abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
+				"continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float",
+				"for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native",
+				"new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super",
+				"switch", "synchronized", "this", "throw", "throws", "transient", "try", "void", "volatile", "while",
+				"var", "module", "requires", "exports"
+			};
+
+			var keywordPattern = "\\b(" + String.join("|", keywords) + ")\\b";
+
+			var semicolonPattern = "\\;";
+
+			var stringPattern = "\"([^\"\\\\]|\\\\.)*\"";
+
+			var commentPattern = "//[^\n]*|/\\*(.|\\R)*?\\*/|/\\*[^\\v]*|^\\h*\\*([^\\v]*|/)";
+
+			var numberPattern = "[0-9]";
+
+			var classPattern = "(?<=\\.)[A-Z]\\w+(?=\\;)|[A-Z]\\w+(?=\\[)|[A-Z]\\w+(?=\\<)|(?<=class\\s)[A-Z]\\w+|(?<=new\\s)[A-Z]\\w+|(?<=extends\\s)[A-Z]\\w+|(?<=implements\\s)[A-Z]\\w+|(?<![a-z]\\w+)[A-Z]\\w+(?=\\.)|[A-Z]\\w+(?=\\s[a-z])";
+
+			var charPattern = "=|\\+|-|\\*|\\/|!|&|\\|:|\\>|\\<|\\?";
+
+			var booleanPattern = "true|false";
+
+			var singleQuoteStringPattern = "'(.*?)'";
+
+			var functionPattern = "[a-z]\\w+(?=\\()";
+
+			var constantPattern = "(?-i)[A-Z]+(?![a-z])";
+
+			var annotationPattern = "@([A-Z]\\w+|[A-Z])";
+
+			pattern = Pattern.compile(
+				"(?<KEYWORD>" + keywordPattern + ")"
+				+ "|(?<SEMICOLON>" + semicolonPattern + ")"
+				+ "|(?<STRING>" + stringPattern + ")"
+				+ "|(?<COMMENT>" + commentPattern + ")"
+				+ "|(?<NUMBER>" + numberPattern + ")"
+				+ "|(?<CLASS>" + classPattern + ")"
+				+ "|(?<CHARS>" + charPattern + ")"
+				+ "|(?<BOOLEAN>" + booleanPattern + ")"
+				+ "|(?<SINGLEQUOTESTRING>" + singleQuoteStringPattern + ")"
+				+ "|(?<FUNCTION>" + functionPattern + ")"
+				+ "|(?<CONSTANT>" + constantPattern + ")"
+				+ "|(?<ANNOTATION>" + annotationPattern + ")"
+			);
+
+			groups.clear();
+
+			groups.put("KEYWORD", "keyword");
+
+			groups.put("SEMICOLON", "semicolon");
+
+			groups.put("STRING", "string");
+
+			groups.put("COMMENT", "comment");
+
+			groups.put("NUMBER", "number");
+
+			groups.put("CLASS", "class");
+
+			groups.put("CHARS", "chars");
+
+			groups.put("BOOLEAN", "boolean");
+
+			groups.put("SINGLEQUOTESTRING", "singleQuoteString");
+
+			groups.put("FUNCTION", "function");
+
+			groups.put("CONSTANT", "constant");
+
+			groups.put("ANNOTATION", "annotation");
+		}
 	}
 
 	public StyleSpans<Collection<String>> highlightSyntax(String text) {
-		var matcher = PATTERN.matcher(text);
+		var matcher = pattern.matcher(text);
 
 		var lastKeywordEnd = 0;
 
 		var styleSpansBuilder = new StyleSpansBuilder<Collection<String>>();
 
-		if (this.programmingLanguage.equals("Java")) {
-			while (matcher.find()) {
-				var styleClass = matcher.group("KEYWORD") != null ? "keyword" :
-					matcher.group("SEMICOLON") != null ? "semicolon" :
-						matcher.group("STRING") != null ? "string" :
-							matcher.group("COMMENT") != null ? "comment" :
-								matcher.group("NUMBER") != null ? "number" :
-									matcher.group("CLASS") != null ? "class" :
-										matcher.group("CHARS") != null ? "chars" :
-											matcher.group("BOOLEAN") != null ? "boolean" :
-												matcher.group("SINGLEQUOTESTRING") != null ? "single_quote_string" :
-													matcher.group("FUNCTION") != null ? "function" :
-														matcher.group("CONSTANT") != null ? "constant" :
-															matcher.group("ANOTATION") != null ? "anotation" :
-															null; assert styleClass != null;
+		String styleClass = null;
 
-				styleSpansBuilder.add(Collections.emptyList(), matcher.start() - lastKeywordEnd);
-
-				styleSpansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
-
-				lastKeywordEnd = matcher.end();
+		while (matcher.find()) {
+			for (var group : groups.keySet()) {
+				if (matcher.group(group) != null) {
+					styleClass = groups.get(group);
+				}
 			}
+
+			styleSpansBuilder.add(Collections.emptyList(), matcher.start() - lastKeywordEnd);
+
+			styleSpansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+
+			lastKeywordEnd = matcher.end();
 		}
 
 		styleSpansBuilder.add(Collections.emptyList(), text.length() - lastKeywordEnd);
