fb5c63595c775965cc299101d727c04f15a933d9
Wenderson <wendersonjunior309@gmail.com>
2025-07-30 14:34:48 -0300

    Mais ajustes no realce de sintaxe C++

--- Highlighter.cpp
+++ Highlighter.cpp
@@ -11,23 +11,9 @@ Highlighter::Highlighter(QTextDocument *parent) : QSyntaxHighlighter(parent) {
 
 	keywordFormat.setFontWeight(QFont::Bold);
 
-	rule.pattern = QRegularExpression(QStringLiteral("\\for\\b|\\balignas\\b|\\balignof\\b|\\band\\b|\\band_eq\\b|\\basm\\b|\\batomic_cancel\\b|\\batomic_commit\\b|\\batomic_noexcept\\b|\\bauto\\b|\\bbitand\\b|\\bbitor\\b|\\bbool\\b|\\bbreak\\b|\\bcase\\b|\\bcatch\\b|\\bchar\\b|\\bchar8_t\\b|\\bchar16_t\\b|\\bchar32_t\\b|\\bclass\\b|\\bcompl\\b|\\bconcept\\b|\\bconst\\b|\\bconsteval\\b|\\bconstexpr\\b|\\bconstinit\\b|\\bconst_cast\\b|\\bcontinue\\b|\\bcontract_assert\\b|\\bco_await\\b|\\bco_return\\b|\\bco_yield\\b|\\bdecltype\\b|\\bdefault\\b|\\bdelete\\b|\\bdo\\b|\\bdouble\\b|\\bdynamic_cast\\b|\\belse\\b|\\benum\\b|\\bexplicit\\b|\\bexport\\b|\\bextern\\b|\\bfalse\\b|\\bfloat\\b|\\bforv\\b|\\bfriend\\b|\\bgoto\\b|\\bif\\b|\\binline\\b|\\bint\\b|\\blong\\b|\\bmutable\\b|\\bnamespace\\b|\\bnew\\b|\\bnoexcept\\b|\\bnot\\b|\\bnot_eq\\b|\\bnullptrv\\b|\\boperator\\b|\\bor\\b|\\bor_eq\\b|\\bprivate\\b|\\bprotected\\b|\\bpublic\\b|\\breflexpr\\b|\\bregister\\b|\\breinterpret_cast\\b|\\brequires\\b|\\breturn\\b|\\bshort\\b|\\bsigned\\b|\\bsizeof\\b|\\bstatic\\b|\\bstatic_assert\\b|\\bstatic_cast\\b|\\bstruct\\b|\\bswitch\\b|\\bsynchronized\\b|\\btemplate\\b|\\bthis\\b|\\bthread_local\\b|\\bthrow\\b|\\btrue\\b|\\btry\\b|\\btypedef\\b|\\btypeid\\b|\\btypename\\b|\\bunion\\b|\\bunsigned\\b|\\busing\\b|\\bvirtual\\b|\\bvoid\\b|\\bvolatile\\b|\\bwchar_t\\b|\\bwhile\\b|\\bxor\\b|\\bxor_eq\\b"));
+	rule.pattern = QRegularExpression(QStringLiteral("\\bfor\\b|\\balignas\\b|\\balignof\\b|\\band\\b|\\band_eq\\b|\\basm\\b|\\batomic_cancel\\b|\\batomic_commit\\b|\\batomic_noexcept\\b|\\bauto\\b|\\bbitand\\b|\\bbitor\\b|\\bbool\\b|\\bbreak\\b|\\bcase\\b|\\bcatch\\b|\\bchar\\b|\\bchar8_t\\b|\\bchar16_t\\b|\\bchar32_t\\b|\\bclass\\b|\\bcompl\\b|\\bconcept\\b|\\bconst\\b|\\bconsteval\\b|\\bconstexpr\\b|\\bconstinit\\b|\\bconst_cast\\b|\\bcontinue\\b|\\bcontract_assert\\b|\\bco_await\\b|\\bco_return\\b|\\bco_yield\\b|\\bdecltype\\b|\\bdefault\\b|\\bdelete\\b|\\bdo\\b|\\bdouble\\b|\\bdynamic_cast\\b|\\belse\\b|\\benum\\b|\\bexplicit\\b|\\bexport\\b|\\bextern\\b|\\bfalse\\b|\\bfloat\\b|\\bforv\\b|\\bfriend\\b|\\bgoto\\b|\\bif\\b|\\binline\\b|\\bint\\b|\\blong\\b|\\bmutable\\b|\\bnamespace\\b|\\bnew\\b|\\bnoexcept\\b|\\bnot\\b|\\bnot_eq\\b|\\bnullptrv\\b|\\boperator\\b|\\bor\\b|\\bor_eq\\b|\\bprivate\\b|\\bprotected\\b|\\bpublic\\b|\\breflexpr\\b|\\bregister\\b|\\breinterpret_cast\\b|\\brequires\\b|\\breturn\\b|\\bshort\\b|\\bsigned\\b|\\bsizeof\\b|\\bstatic\\b|\\bstatic_assert\\b|\\bstatic_cast\\b|\\bstruct\\b|\\bswitch\\b|\\bsynchronized\\b|\\btemplate\\b|\\bthis\\b|\\bthread_local\\b|\\bthrow\\b|\\btrue\\b|\\btry\\b|\\btypedef\\b|\\btypeid\\b|\\btypename\\b|\\bunion\\b|\\bunsigned\\b|\\busing\\b|\\bvirtual\\b|\\bvoid\\b|\\bvolatile\\b|\\bwchar_t\\b|\\bwhile\\b|\\bxor\\b|\\bxor_eq\\b"));
 
 	rule.format = keywordFormat;
-
-	highlightingRules.append(rule);
-
-	QTextCharFormat classFormat;
-
-	QColor purple(189, 147, 249);
-
-	classFormat.setForeground(purple);
-
-	classFormat.setFontWeight(QFont::Bold);
-
-	rule.pattern = QRegularExpression(QStringLiteral("(?<!([a-z]|::))[A-Z][A-Za-z]+"));
-
-	rule.format = classFormat;
 
 	highlightingRules.append(rule);
 
@@ -43,27 +29,61 @@ Highlighter::Highlighter(QTextDocument *parent) : QSyntaxHighlighter(parent) {
 
 	highlightingRules.append(rule);
 
-	QTextCharFormat quotationFormat;
-
-	QColor yellow(241, 250, 140);
-
-	quotationFormat.setForeground(yellow);
-
-	rule.pattern = QRegularExpression(QStringLiteral("\".*\""));
-
-	rule.format = quotationFormat;
-
-	highlightingRules.append(rule);
-
 	QTextCharFormat functionFormat;
 
 	QColor green(80, 250, 123);
 
 	functionFormat.setForeground(green);
 
-	rule.pattern = QRegularExpression(QStringLiteral("\\b[A-Za-z0-9_]+(?=\\()"));
+	rule.pattern = QRegularExpression(QStringLiteral("[A-Za-z0-9_]+(?=\\()"));
 
 	rule.format = functionFormat;
+
+	highlightingRules.append(rule);
+
+	QTextCharFormat classFormat;
+
+	QColor purple(189, 147, 249);
+
+	classFormat.setForeground(purple);
+
+	rule.pattern = QRegularExpression(QStringLiteral("(?<!([a-z]|::))[A-Z][A-Za-z]+"));
+
+	rule.format = classFormat;
+
+	highlightingRules.append(rule);
+
+	QTextCharFormat operatorFormat;
+
+	QColor orange(255, 184, 108);
+
+	operatorFormat.setForeground(orange);
+
+	rule.pattern = QRegularExpression(QStringLiteral("\\+|-|\\*|\\/|=|!|&|\\|"));
+
+	rule.format = operatorFormat;
+
+	highlightingRules.append(rule);
+
+	QTextCharFormat semicolonFormat;
+
+	semicolonFormat.setForeground(Qt::lightGray);
+
+	rule.pattern = QRegularExpression(QStringLiteral(";"));
+
+	rule.format = semicolonFormat;
+
+	highlightingRules.append(rule);
+
+	QTextCharFormat stringFormat;
+
+	QColor yellow(241, 250, 140);
+
+	stringFormat.setForeground(yellow);
+
+	rule.pattern = QRegularExpression(QStringLiteral("\".*\""));
+
+	rule.format = stringFormat;
 
 	highlightingRules.append(rule);
 
